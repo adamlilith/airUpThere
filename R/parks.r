4533b0c6e4dfb3190893138a72f099575111d362
Adam B. Smith <adam.smith@mobot.org>
2024-01-13 01:44:37 -0600

    Create parks.r

---
+++ R/parks.r
@@ -0,0 +1,19 @@
+#' Outlines of two parks in Saint Louis, Missouri
+#'
+#' @description This file is a `SpatVector` outline of two parks in Saint Louis, Missouri. Forest Park is the largest municipal park in the US, and Tower Grove Park the largest Victorian park.
+#'
+#' @source [City of Saint Louis](https://www.stlouis-mo.gov/)
+#'
+#' @seealso [airExample()]
+#' 
+#' @examples
+#' 
+#' parks <- airExample('parks')
+#' parks
+#' plot(parks)
+#' 
+#' @format `SpatVector`
+#' @rdname parks
+#' @docType data
+#' @export
+'parks'
