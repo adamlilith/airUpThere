579c29fc5f7440e963efee83558a6da888a30718
Adam B. Smith <adam.smith@mobot.org>
2024-01-13 01:53:05 -0600

    Create wgs84.r

---
+++ R/wgs84.r
@@ -0,0 +1,16 @@
+#' Well-Known Text string for the WGS84 Datum
+#'
+#' @description This is the Well-Known Text (WKT) string for the World Geodetic System 1984 (WGS84).
+#'
+#' @seealso [nad83]
+#' 
+#' @examples
+#' 
+#' data(wgs84)
+#' wgs84
+#' cat(wgs84)
+#' 
+#' @format `character`
+#' @docType data
+#' @export
+'wgs84'
