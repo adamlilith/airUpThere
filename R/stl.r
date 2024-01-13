27a1b373c415edda360713a8d6c93c795e6d9743
Adam B. Smith <adam.smith@mobot.org>
2024-01-13 01:52:58 -0600

    Create stl.r

---
+++ R/stl.r
@@ -0,0 +1,18 @@
+#' Outline of the City of Saint Louis, Missouri, USA
+#'
+#' @description This file is a `SpatVector` outline of the City of Saint Louis, Missouri, USA.
+#'
+#' @source [City of Saint Louis](https://www.stlouis-mo.gov/)
+#'
+#' @seealso [airExample()]
+#' 
+#' @examples
+#' 
+#' stl <- airExample('stl')
+#' stl
+#' plot(stl)
+#' 
+#' @docType data
+#' @format `SpatVector`
+#' @export
+'stl'
