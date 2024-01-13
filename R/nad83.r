8d769aaa90144bbf3de2c4c48a800291e6b3bd0a
Adam B. Smith <adam.smith@mobot.org>
2024-01-13 01:52:53 -0600

    Create nad83.r

---
+++ R/nad83.r
@@ -0,0 +1,15 @@
+#' Well-Known Text string for the NAD83 Datum
+#'
+#' @description This is the Well-Known Text (WKT) string for the North American Datum 1983 (NAD83).
+#'
+#' @seealso [wgs84]
+#' 
+#' @examples
+#' 
+#' data(nad83)
+#' nad83
+#' cat(nad83)
+#' 
+#' @format `character`
+#' @export
+'nad83'
