62c62e3abcc63961063fecffbeada724873dbe5d
Adam B. Smith <adam.smith@mobot.org>
2024-01-13 01:53:01 -0600

    Create trees.r

---
+++ R/trees.r
@@ -0,0 +1,18 @@
+#' Trees in the City of Saint Louis, Missouri, USA
+#'
+#' @description This file is a `SpatVector` of a random sample of the locations of trees in Saint Louis, Missouri, USA.
+#'
+#' @source [City of Saint Louis](https://www.treesouis-mo.gov/)
+#'
+#' @seealso [airExample()]
+#' 
+#' @examples
+#' 
+#' trees <- airExample('trees')
+#' trees
+#' plot(trees)
+#' 
+#' @docType data
+#' @format `SpatVector`
+#' @export
+'trees'
